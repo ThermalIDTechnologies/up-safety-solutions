06f3107be1f47d67c5483d7ab42653a02ca91587
NRD89 <ndaltondev@gmail.com>
2019-05-22 17:30:24 -0700

    changed image query maxWidth to 402 on index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -149,7 +149,7 @@ export const pageQuery = graphql`
             image {
               localFile {
                 childImageSharp {
-                  fluid(maxWidth: 300, quality: 90) {
+                  fluid(maxWidth: 402, quality: 90) {
                     ...GatsbyImageSharpFluid_withWebp_tracedSVG
                   }
                 }
